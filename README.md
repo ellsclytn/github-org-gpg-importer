9e75ba171cf9840abd6f28011c5c32727948a83f
Ellis Clayton <ellis@ellis.codes>
2020-01-29 21:41:50 +1100

    Inital commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# GitHub Public Key Getter
+
+**Import GPG public keys for all the users in a specified GitHub organisation, and save the keys to files as well**
+
+---
+
+## Requirements
+
+* Bash
+* Curl
+* Jq
+* GPG
+
+## Usage
+
+`./get-gpg-keys.sh` and follow the prompts
---
+++ get-gpg-keys.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -euo pipefail
+
+echo "Enter your GitHub username"
+read -r github_username
+echo "Enter your GitHub Access Token (read:org scope required)"
+read -r github_token
+echo "Enter the GitHub organisation slug (as it appears in URLs)"
+read -r github_org
+
+github_request () {
+  curl -su "$github_username:$github_token" "https://api.github.com/$1"
+}
+
+members=$(github_request "orgs/$github_org/members")
+users=$(echo "$members" | jq -r '.[].login')
+
+for user in $users; do
+  user_key_response=$(github_request "users/$user/gpg_keys")
+  raw_key="$(echo "$user_key_response" | jq -r '.[0].raw_key')"
+
+  if [[ "$raw_key" != "null" ]]; then
+    # I don't know. GitHub gives annoying \r\n stuff and it's annoying.
+    # Did I mention it's annoying? It's annoying.
+    echo "$user_key_response" | jq -r '.[0].raw_key' > "keys/$user.pub"
+    gpg --import "keys/$user.pub"
+  fi
+done
